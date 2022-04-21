93c65743dbb154c06908974f8662f01009772db1
jrdnbrkfld <jrdnbrkfld@icloud.com>
2022-04-21 22:22:58 +0000

    added title screen with the start node text

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+print("             ################")
+print("            ##              ##")
+print("           ###              ###")
+print("          ####    Title     ####")
+print("           ###              ###")
+print("            ##              ##")
+print("             ################")
+print()
+print()
+print("""You sprint off the bus.
+Just your luck that it was running late on the night of your first shift.
+The time is 21:56, not as early as you would have liked to have arrived.
+You approach the staff entrance to the side of the building.
+Your new boss is standing there, looking quite annoyed.\n""")
+startGame = input("Would you like to start your shift? (Y/N)")
+if startGame == 'n' or startGame == 'N':
+    print("The boss is visually furious with you. Maybe some other time?")
+elif startGame == 'y' or startGame == 'Y':
+    intro()
