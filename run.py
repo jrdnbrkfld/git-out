ddb8d5d69d23937b407ad173f17572f48578a893
jrdnbrkfld <jrdnbrkfld@icloud.com>
2022-04-27 19:31:53 +0000

    added cyan color to all the options to choose from

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ Just your luck that it was running late on the night of your first shift.
 The time is 21:56, not as early as you would have liked to have arrived.
 You approach the staff entrance to the side of the building.
 Your new boss is standing there, looking quite worried.\n""")
-    startGame = input("Would you like to start your shift? (Y/N)\n")
+    startGame = input(Fore.CYAN + Style.BRIGHT + "Would you like to start your shift? (Y/N)\n")
     if startGame == 'n' or startGame == 'N':
         print("The boss is visually furious with you. Maybe some other time?")
     elif startGame == 'y' or startGame == 'Y':
@@ -34,7 +34,7 @@ On his way out, the boss places a handwritten note in your hand.
 'Please make sure you read this when I`ve left', he says.
 He exits the door, locking it and making his way to his car.""")
     print()
-    firstPath = input("Do you want to read the note? (Y/N)\n")
+    firstPath = input(Fore.CYAN + Style.BRIGHT + "Do you want to read the note? (Y/N)\n")
     if firstPath == 'y' or firstPath == 'Y':
         path1()
     elif firstPath == 'n' or firstPath == 'N':
@@ -54,16 +54,15 @@ def path1():
 The writing is barely legible, but you persevere.
 Your face goes pale, your hairs stick up on your arms.
 The note reads…
-“I'm so sorry, I had no other choice.
-She is hungry.”""")
-    print()
-    pathOneA = input("""What would you like to do?
+“I'm so sorry, I had no other choice.y
+She is hungry.”
+What would you like to do?
 
 Path 1: Run to the door and try to get out.
 Path 2: Hide underneath the desk.
-Path 3: Make your way into the main storage halls.
-
-Make your choice. (1/2/3)\n""")
+Path 3: Make your way into the main storage halls.""")
+    print()
+    pathOneA = input(Fore.CYAN + Style.BRIGHT + "Make your choice. (1/2/3)\n")
     if pathOneA == '1':
         path1A()
     else:
@@ -82,14 +81,13 @@ you hear a guttural moan from down the hallway.
 “Why won't this damn door open?!”, you scream.
 You begin to hear footsteps from around the corner.
 Growing louder with each stride.
-Then you remember, the boss locked the door…""")
-    print()
-    pathOneAOne = input("""With seconds until whatever is approaching you appears, you must choose!
+Then you remember, the boss locked the door…
+With seconds until whatever is approaching you appears, you must choose!
 
 Path 1: Smash through the glass.
-Path 2: Run back into the office and hide.
-
-Make your choice. (1/2)\n""")
+Path 2: Run back into the office and hide.""")
+    print()
+    pathOneAOne = input(Fore.CYAN + Style.BRIGHT + "Make your choice. (1/2)\n")
     if pathOneAOne == '1':
         print()
         print("""You grab the fire extinguisher nearby and smash through the glass.
