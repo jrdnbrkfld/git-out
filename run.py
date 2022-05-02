a5ed6b27fa7c81871b1af4cc9ceb3236bf68daff
jrdnbrkfld <jrdnbrkfld@icloud.com>
2022-05-02 21:32:33 +0000

    creat read_file() on line 204 that prints txt file added read_file() to functions

--- run.py
+++ run.py
@@ -1,25 +1,16 @@
 from sys import exit
 import colorama
-from colorama import Fore, Style
+from colorama import Fore, Back, Style
 colorama.init(autoreset=True)
 
 
 # beginning function to start the game
 def title():
-    print()
-    f = open("assets/story/title.txt", 'r')
-    print(f.read())
-    print()
-    startGame = input(
-        Fore.CYAN + Style.BRIGHT +
-        "Would you like to start your shift? (Y/N)\n")
-    if startGame.lower() == 'n':
-        print()
-        print(
-            Style.RESET_ALL +
-            "The boss is visually furious with you. Maybe some other time...")
-        main()
-    elif startGame.lower() == 'y':
+    read_file("assets/story/title.txt")
+    user_choice = input(Fore.CYAN + Style.BRIGHT + "Would you like to start your shift? (Y/N)\n" + Style.RESET_ALL)
+    if user_choice.lower() == 'n':
+        read_file("assets/story/no.txt")
+    elif user_choice.lower() == 'y':
         intro()
     else:
         error()
@@ -28,16 +19,11 @@ def title():
 
 # intro function
 def intro():
-    print()
-    f = open("assets/story/intro.txt", 'r')
-    print(f.read())
-    print()
-    firstPath = input(
-        Fore.CYAN + Style.BRIGHT +
-        "Do you want to read the note? (Y/N)\n")
-    if firstPath.lower() == 'y':
+    read_file("assets/story/intro.txt")
+    user_choice = input(Fore.CYAN + Style.BRIGHT + "Do you want to read the note? (Y/N)\n" + Style.RESET_ALL)
+    if user_choice.lower() == 'y':
         path1()
-    elif firstPath.lower() == 'n':
+    elif user_choice.lower() == 'n':
         path2()
     else:
         error()
@@ -46,17 +32,13 @@ def intro():
 
 # the path in which you read the note
 def path1():
-    print()
-    f = open("assets/story/path_one.txt", 'r')
-    print(f.read())
-    print()
-    path_one_a = input(
-        Fore.CYAN + Style.BRIGHT + "Make your choice. (1/2/3)\n")
-    if path_one_a == '1':
+    read_file("assets/story/path_one.txt")
+    user_choice = input(Fore.CYAN + Style.BRIGHT + "Make your choice. (1/2/3)\n" + Style.RESET_ALL)
+    if user_choice == '1':
         path1A()
-    elif path_one_a == '2':
+    elif user_choice == '2':
         path1B()
-    elif path_one_a == '3':
+    elif user_choice == '3':
         path1C()
     else:
         error()
@@ -65,20 +47,13 @@ def path1():
 
 # the path in which you try to exit through the door
 def path1A():
-    print()
-    f = open("assets/story/path_one_a.txt", 'r')
-    print(f.read())
-    print()
-    path1A1 = input(Fore.CYAN + Style.BRIGHT + "Make your choice. (1/2)\n")
-    if path1A1 == '1':
-        print()
-        f = open("assets/story/path_one_a_win.txt", 'r')
-        print(f.read())
+    read_file("assets/story/path_one_a.txt")
+    user_choice = input(Fore.CYAN + Style.BRIGHT + "Make your choice. (1/2)\n" + Style.RESET_ALL)
+    if user_choice == '1':
+        read_file("assets/story/path_one_a_win.txt")
         win()
-    elif path1A1 == '2':
-        print()
-        f = open("assets/story/path_one_a_lose.txt", 'r')
-        print(f.read())
+    elif user_choice == '2':
+        read_file("assets/story/path_one_a_lose.txt")
         lose()
     else:
         error()
@@ -87,20 +62,13 @@ def path1A():
 
 # the path where you run back into the office and hide
 def path1B():
-    print()
-    f = open("assets/story/path_one_b.txt", 'r')
-    print(f.read())
-    print()
-    path1B1 = input(Fore.CYAN + Style.BRIGHT + "Make your choice. (1/2)\n")
-    if path1B1 == '1':
-        print()
-        f = open("assets/story/path_one_b_lose.txt", 'r')
-        print(f.read())
+    read_file("assets/story/path_one_b.txt")
+    user_choice = input(Fore.CYAN + Style.BRIGHT + "Make your choice. (1/2)\n" + Style.RESET_ALL)
+    if user_choice == '1':
+        read_file("assets/story/path_one_b_lose.txt")
         lose()
-    elif path1B1 == '2':
-        print()
-        f = open("assets/story/path_one_b_win.txt", 'r')
-        print(f.read())
+    elif user_choice == '2':
+        read_file("assets/story/path_one_b_win.txt")
         win()
     else:
         error()
@@ -109,20 +77,13 @@ def path1B():
 
 # the path where you explore the main halls
 def path1C():
-    print()
-    f = open("assets/story/path_one_c.txt", 'r')
-    print(f.read())
-    print()
-    path1C1 = input(Fore.CYAN + Style.BRIGHT + "Make your choice. (1/2)\n")
-    if path1C1 == '1':
-        print()
-        f = open("assets/story/path_one_c_win.txt", 'r')
-        print(f.read())
+    read_file("assets/story/path_one_c.txt")
+    user_choice = input(Fore.CYAN + Style.BRIGHT + "Make your choice. (1/2)\n" + Style.RESET_ALL)
+    if user_choice == '1':
+        read_file("assets/story/path_one_c_win.txt")
         win()
-    elif path1C1 == '2':
-        print()
-        f = open("assets/story/path_one_c_lose.txt", 'r')
-        print(f.read())
+    elif user_choice == '2':
+        read_file("assets/story/path_one_c_lose.txt", 'r')
         lose()
     else:
         error()
@@ -131,17 +92,14 @@ def path1C():
 
 # the path where you don't read the note
 def path2():
-    print()
-    f = open("assets/story/path_two.txt", 'r')
-    print(f.read())
-    print()
-    path_two_a = input(
-        Fore.CYAN + Style.BRIGHT + "Make your choice. (1/2/3)\n")
-    if path_two_a == '1':
+    read_file("assets/story/path_two.txt")
+    user_choice = input(
+        Fore.CYAN + Style.BRIGHT + "Make your choice. (1/2/3)\n" + Style.RESET_ALL)
+    if user_choice == '1':
         path2A()
-    elif path_two_a == '2':
+    elif user_choice == '2':
         path2B()
-    elif path_two_a == '3':
+    elif user_choice == '3':
         path2C()
     else:
         error()
@@ -150,20 +108,13 @@ def path2():
 
 # the path where you do your first patrol
 def path2A():
-    print()
-    f = open("assets/story/path_two_a.txt", 'r')
-    print(f.read())
-    print()
-    path2A1 = input(Fore.CYAN + Style.BRIGHT + "Make your choice. (1/2)\n")
-    if path2A1 == '1':
-        print()
-        f = open("assets/story/path_two_a_win.txt", 'r')
-        print(f.read())
+    read_file("assets/story/path_two_a.txt")
+    user_choice = input(Fore.CYAN + Style.BRIGHT + "Make your choice. (1/2)\n" + Style.RESET_ALL)
+    if user_choice == '1':
+        read_file("assets/story/path_two_a_win.txt")
         win()
-    elif path2A1 == '2':
-        print()
-        f = open("assets/story/path_two_a_lose.txt", 'r')
-        print(f.read())
+    elif user_choice == '2':
+        read_file("assets/story/path_two_a_lose.txt")
         lose()
     else:
         error()
@@ -172,20 +123,13 @@ def path2A():
 
 # the path where you boil the kettle
 def path2B():
-    print()
-    f = open("assets/story/path_two_b.txt", 'r')
-    print(f.read())
-    print()
-    path2B1 = input(Fore.CYAN + Style.BRIGHT + "Make your choice. (1/2)\n")
-    if path2B1 == '1':
-        print()
-        f = open("assets/story/path_two_b_win.txt", 'r')
-        print(f.read())
+    read_file("assets/story/path_two_b.txt")
+    user_choice = input(Fore.CYAN + Style.BRIGHT + "Make your choice. (1/2)\n" + Style.RESET_ALL)
+    if user_choice == '1':
+        read_file("assets/story/path_two_b_win.txt")
         win()
-    elif path2B1 == '2':
-        print()
-        f = open("assets/story/path_two_b_lose.txt", 'r')
-        print(f.read())
+    elif user_choice == '2':
+        read_file("assets/story/path_two_b_lose.txt")
         lose()
     else:
         error()
@@ -193,20 +137,13 @@ def path2B():
 
 
 def path2C():
-    print()
-    f = open("assets/story/path_two_c.txt", 'r')
-    print(f.read())
-    print()
-    path2C1 = input(Fore.CYAN + Style.BRIGHT + "Make your choice. (1/2)\n")
-    if path2C1 == '1':
-        print()
-        f = open("assets/story/path_two_c_win.txt", 'r')
-        print(f.read())
+    read_file("assets/story/path_two_c.txt")
+    user_choice = input(Fore.CYAN + Style.BRIGHT + "Make your choice. (1/2)\n" + Style.RESET_ALL)
+    if user_choice == '1':
+        read_file("assets/story/path_two_c_win.txt")
         win()
-    elif path2C1 == '2':
-        print()
-        f = open("assets/story/path_two_c_lose.txt", 'r')
-        print(f.read())
+    elif user_choice == '2':
+        read_file("assets/story/path_two_c_lose.txt")
         lose()
     else:
         error()
@@ -217,9 +154,7 @@ def path2C():
 def win():
     f = open("assets/ASCII/win.txt", 'r')
     print(Fore.GREEN + Style.BRIGHT + f.read())
-    retry = input(
-        Fore.CYAN + Style.BRIGHT +
-        "Would you like to try again? Y/N\n")
+    retry = input(Fore.CYAN + Style.BRIGHT + "Would you like to try again? Y/N\n" + Style.RESET_ALL)
     if retry.lower() == 'y':
         main()
     elif retry.lower() == 'n':
@@ -233,9 +168,7 @@ def win():
 def lose():
     f = open("assets/ASCII/lose.txt", 'r')
     print(Fore.RED + Style.BRIGHT + f.read())
-    retry = input(
-        Fore.CYAN + Style.BRIGHT +
-        "Would you like to try again? Y/N\n")
+    retry = input(Fore.CYAN + Style.BRIGHT + "Would you like to try again? Y/N\n" + Style.RESET_ALL)
     if retry.lower() == 'y':
         main()
     elif retry.lower() == 'n':
@@ -251,18 +184,12 @@ def main():
     print(Fore.CYAN + Style.BRIGHT + f.read())
     f = open("assets/story/credits.txt", 'r')
     print(Fore.GREEN + Style.BRIGHT + f.read())
-    f = open("assets/story/main.txt", 'r')
-    print(f.read())
-    print()
-    begin = input(
-        Fore.CYAN + Style.BRIGHT +
-        "Would you like to play the game? (Y/N)\n")
-    if begin.lower() == 'y':
+    read_file("assets/story/main.txt")
+    user_choice = input(Fore.CYAN + Style.BRIGHT + "Would you like to play the game? (Y/N)\n" + Style.RESET_ALL)
+    if user_choice.lower() == "y":
         title()
-    elif begin.lower() == 'n':
-        print()
-        print("That's too bad, maybe some other time.\n")
-        exit()
+    elif user_choice.lower() == "n":
+        read_file("assets/story/no.txt")
     else:
         error()
         main()
@@ -272,6 +199,12 @@ def main():
 def error():
     f = open("assets/story/error.txt", 'r')
     print(Fore.RED + Style.BRIGHT + f.read())
+
+
+def read_file(current_story_file):
+    with open(current_story_file) as file:
+        file_text = file.read()
+        print(file_text)
 
 
 main()
